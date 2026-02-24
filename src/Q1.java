b769f1ece449ff9acc953becf19f37974277bef5
Ewerton <ewerton.rocha@academico.ifpb.edu.br>
2026-02-24 20:00:48 -0300

    primeiro commit

---
+++ src/Q1.java
@@ -0,0 +1,24 @@
+public class Q1 {
+    public static void main(String[] args) {
+        // aqui ele pega o valor minimo e maximo de cada tipo primitivo escolhidos
+        System.out.println("valor maximo byte: " + Byte.MAX_VALUE + " valor minimo: " + Byte.MIN_VALUE);
+        System.out.println("valor maximo short: " + Short.MAX_VALUE + " valor minimo: " + Short.MIN_VALUE);
+        System.out.println("valor maximo int: " + Integer.MAX_VALUE + " valor minimo: " + Integer.MIN_VALUE);
+
+        // nos cassos abaixo acontece overflow ultrapassa o valor maximo de cada caso
+        byte a = Byte.MAX_VALUE;
+        byte overflowByte = (byte)(a + 1);
+        System.out.println("Byte MAX + 1: " + overflowByte);
+
+        // ele pega o maior numero do tipo e adiciona um levando o numero a ser o min dele
+        short b = Short.MAX_VALUE;
+        short overflowShort = (short)(b + 1);
+        System.out.println("Short MAX + 1: " + overflowShort);
+
+        int c = Byte.MAX_VALUE;
+        int overflowInt = (int)(c + 1);
+        System.out.println("Int MAX + 1: " + overflowByte);
+
+
+    }
+}
